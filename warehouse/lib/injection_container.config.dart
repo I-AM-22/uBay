03f3d6a716ee97250c7feba50880a37180f528f9
Majed Alaswad <alaswadmajed389@gmail.com>
2023-05-21 23:43:55 +0300

    add[warehouse](App):connect api with login signup page

---
+++ warehouse/lib/injection_container.config.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_lambdas
+// ignore_for_file: lines_longer_than_80_chars
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+import 'package:shared_preferences/shared_preferences.dart' as _i3;
+import 'package:warehouse/injection_container.dart' as _i4;
+
+extension GetItInjectableX on _i1.GetIt {
+  // initializes the registration of main-scope dependencies inside of GetIt
+  _i1.GetIt init({
+    String? environment,
+    _i2.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i2.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    final registerModule = _$RegisterModule();
+    gh.factoryAsync<_i3.SharedPreferences>(
+        () => registerModule.sharedPreferences);
+    return this;
+  }
+}
+
+class _$RegisterModule extends _i4.RegisterModule {}
