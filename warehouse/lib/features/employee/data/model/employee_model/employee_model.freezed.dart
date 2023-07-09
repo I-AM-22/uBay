992e11e40491fde6f91d72fae086aef5d21778f9
majed alaswad <alaswadmajed389@gmail.com>
2023-07-09 17:14:24 +0300

    (APP)[warehouse]:add bloc for employee

---
+++ warehouse/lib/features/employee/data/model/employee_model/employee_model.freezed.dart
@@ -0,0 +1,673 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'employee_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+EmployeeModel _$EmployeeModelFromJson(Map<String, dynamic> json) {
+  return _EmployeeModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$EmployeeModel {
+  String get name => throw _privateConstructorUsedError;
+  String get email => throw _privateConstructorUsedError;
+  String get photo => throw _privateConstructorUsedError;
+  Store get store => throw _privateConstructorUsedError;
+  String get address => throw _privateConstructorUsedError;
+  DateTime get createdAt => throw _privateConstructorUsedError;
+  DateTime get updatedAt => throw _privateConstructorUsedError;
+  String get role => throw _privateConstructorUsedError;
+  String get id => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $EmployeeModelCopyWith<EmployeeModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $EmployeeModelCopyWith<$Res> {
+  factory $EmployeeModelCopyWith(
+          EmployeeModel value, $Res Function(EmployeeModel) then) =
+      _$EmployeeModelCopyWithImpl<$Res, EmployeeModel>;
+  @useResult
+  $Res call(
+      {String name,
+      String email,
+      String photo,
+      Store store,
+      String address,
+      DateTime createdAt,
+      DateTime updatedAt,
+      String role,
+      String id});
+
+  $StoreCopyWith<$Res> get store;
+}
+
+/// @nodoc
+class _$EmployeeModelCopyWithImpl<$Res, $Val extends EmployeeModel>
+    implements $EmployeeModelCopyWith<$Res> {
+  _$EmployeeModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? email = null,
+    Object? photo = null,
+    Object? store = null,
+    Object? address = null,
+    Object? createdAt = null,
+    Object? updatedAt = null,
+    Object? role = null,
+    Object? id = null,
+  }) {
+    return _then(_value.copyWith(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      email: null == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      photo: null == photo
+          ? _value.photo
+          : photo // ignore: cast_nullable_to_non_nullable
+              as String,
+      store: null == store
+          ? _value.store
+          : store // ignore: cast_nullable_to_non_nullable
+              as Store,
+      address: null == address
+          ? _value.address
+          : address // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      updatedAt: null == updatedAt
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      role: null == role
+          ? _value.role
+          : role // ignore: cast_nullable_to_non_nullable
+              as String,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $StoreCopyWith<$Res> get store {
+    return $StoreCopyWith<$Res>(_value.store, (value) {
+      return _then(_value.copyWith(store: value) as $Val);
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$_EmployeeModelCopyWith<$Res>
+    implements $EmployeeModelCopyWith<$Res> {
+  factory _$$_EmployeeModelCopyWith(
+          _$_EmployeeModel value, $Res Function(_$_EmployeeModel) then) =
+      __$$_EmployeeModelCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String name,
+      String email,
+      String photo,
+      Store store,
+      String address,
+      DateTime createdAt,
+      DateTime updatedAt,
+      String role,
+      String id});
+
+  @override
+  $StoreCopyWith<$Res> get store;
+}
+
+/// @nodoc
+class __$$_EmployeeModelCopyWithImpl<$Res>
+    extends _$EmployeeModelCopyWithImpl<$Res, _$_EmployeeModel>
+    implements _$$_EmployeeModelCopyWith<$Res> {
+  __$$_EmployeeModelCopyWithImpl(
+      _$_EmployeeModel _value, $Res Function(_$_EmployeeModel) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? email = null,
+    Object? photo = null,
+    Object? store = null,
+    Object? address = null,
+    Object? createdAt = null,
+    Object? updatedAt = null,
+    Object? role = null,
+    Object? id = null,
+  }) {
+    return _then(_$_EmployeeModel(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      email: null == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      photo: null == photo
+          ? _value.photo
+          : photo // ignore: cast_nullable_to_non_nullable
+              as String,
+      store: null == store
+          ? _value.store
+          : store // ignore: cast_nullable_to_non_nullable
+              as Store,
+      address: null == address
+          ? _value.address
+          : address // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      updatedAt: null == updatedAt
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      role: null == role
+          ? _value.role
+          : role // ignore: cast_nullable_to_non_nullable
+              as String,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_EmployeeModel implements _EmployeeModel {
+  _$_EmployeeModel(
+      {required this.name,
+      required this.email,
+      required this.photo,
+      required this.store,
+      required this.address,
+      required this.createdAt,
+      required this.updatedAt,
+      required this.role,
+      required this.id});
+
+  factory _$_EmployeeModel.fromJson(Map<String, dynamic> json) =>
+      _$$_EmployeeModelFromJson(json);
+
+  @override
+  final String name;
+  @override
+  final String email;
+  @override
+  final String photo;
+  @override
+  final Store store;
+  @override
+  final String address;
+  @override
+  final DateTime createdAt;
+  @override
+  final DateTime updatedAt;
+  @override
+  final String role;
+  @override
+  final String id;
+
+  @override
+  String toString() {
+    return 'EmployeeModel(name: $name, email: $email, photo: $photo, store: $store, address: $address, createdAt: $createdAt, updatedAt: $updatedAt, role: $role, id: $id)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_EmployeeModel &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.email, email) || other.email == email) &&
+            (identical(other.photo, photo) || other.photo == photo) &&
+            (identical(other.store, store) || other.store == store) &&
+            (identical(other.address, address) || other.address == address) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt) &&
+            (identical(other.updatedAt, updatedAt) ||
+                other.updatedAt == updatedAt) &&
+            (identical(other.role, role) || other.role == role) &&
+            (identical(other.id, id) || other.id == id));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, name, email, photo, store,
+      address, createdAt, updatedAt, role, id);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_EmployeeModelCopyWith<_$_EmployeeModel> get copyWith =>
+      __$$_EmployeeModelCopyWithImpl<_$_EmployeeModel>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_EmployeeModelToJson(
+      this,
+    );
+  }
+}
+
+abstract class _EmployeeModel implements EmployeeModel {
+  factory _EmployeeModel(
+      {required final String name,
+      required final String email,
+      required final String photo,
+      required final Store store,
+      required final String address,
+      required final DateTime createdAt,
+      required final DateTime updatedAt,
+      required final String role,
+      required final String id}) = _$_EmployeeModel;
+
+  factory _EmployeeModel.fromJson(Map<String, dynamic> json) =
+      _$_EmployeeModel.fromJson;
+
+  @override
+  String get name;
+  @override
+  String get email;
+  @override
+  String get photo;
+  @override
+  Store get store;
+  @override
+  String get address;
+  @override
+  DateTime get createdAt;
+  @override
+  DateTime get updatedAt;
+  @override
+  String get role;
+  @override
+  String get id;
+  @override
+  @JsonKey(ignore: true)
+  _$$_EmployeeModelCopyWith<_$_EmployeeModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+Store _$StoreFromJson(Map<String, dynamic> json) {
+  return _Store.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Store {
+  String get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  String get address => throw _privateConstructorUsedError;
+  City get city => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $StoreCopyWith<Store> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $StoreCopyWith<$Res> {
+  factory $StoreCopyWith(Store value, $Res Function(Store) then) =
+      _$StoreCopyWithImpl<$Res, Store>;
+  @useResult
+  $Res call({String id, String name, String address, City city});
+
+  $CityCopyWith<$Res> get city;
+}
+
+/// @nodoc
+class _$StoreCopyWithImpl<$Res, $Val extends Store>
+    implements $StoreCopyWith<$Res> {
+  _$StoreCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? address = null,
+    Object? city = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      address: null == address
+          ? _value.address
+          : address // ignore: cast_nullable_to_non_nullable
+              as String,
+      city: null == city
+          ? _value.city
+          : city // ignore: cast_nullable_to_non_nullable
+              as City,
+    ) as $Val);
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $CityCopyWith<$Res> get city {
+    return $CityCopyWith<$Res>(_value.city, (value) {
+      return _then(_value.copyWith(city: value) as $Val);
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$_StoreCopyWith<$Res> implements $StoreCopyWith<$Res> {
+  factory _$$_StoreCopyWith(_$_Store value, $Res Function(_$_Store) then) =
+      __$$_StoreCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String id, String name, String address, City city});
+
+  @override
+  $CityCopyWith<$Res> get city;
+}
+
+/// @nodoc
+class __$$_StoreCopyWithImpl<$Res> extends _$StoreCopyWithImpl<$Res, _$_Store>
+    implements _$$_StoreCopyWith<$Res> {
+  __$$_StoreCopyWithImpl(_$_Store _value, $Res Function(_$_Store) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? address = null,
+    Object? city = null,
+  }) {
+    return _then(_$_Store(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      address: null == address
+          ? _value.address
+          : address // ignore: cast_nullable_to_non_nullable
+              as String,
+      city: null == city
+          ? _value.city
+          : city // ignore: cast_nullable_to_non_nullable
+              as City,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Store implements _Store {
+  _$_Store(
+      {required this.id,
+      required this.name,
+      required this.address,
+      required this.city});
+
+  factory _$_Store.fromJson(Map<String, dynamic> json) =>
+      _$$_StoreFromJson(json);
+
+  @override
+  final String id;
+  @override
+  final String name;
+  @override
+  final String address;
+  @override
+  final City city;
+
+  @override
+  String toString() {
+    return 'Store(id: $id, name: $name, address: $address, city: $city)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Store &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.address, address) || other.address == address) &&
+            (identical(other.city, city) || other.city == city));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, name, address, city);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_StoreCopyWith<_$_Store> get copyWith =>
+      __$$_StoreCopyWithImpl<_$_Store>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_StoreToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Store implements Store {
+  factory _Store(
+      {required final String id,
+      required final String name,
+      required final String address,
+      required final City city}) = _$_Store;
+
+  factory _Store.fromJson(Map<String, dynamic> json) = _$_Store.fromJson;
+
+  @override
+  String get id;
+  @override
+  String get name;
+  @override
+  String get address;
+  @override
+  City get city;
+  @override
+  @JsonKey(ignore: true)
+  _$$_StoreCopyWith<_$_Store> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+City _$CityFromJson(Map<String, dynamic> json) {
+  return _City.fromJson(json);
+}
+
+/// @nodoc
+mixin _$City {
+  String get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $CityCopyWith<City> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CityCopyWith<$Res> {
+  factory $CityCopyWith(City value, $Res Function(City) then) =
+      _$CityCopyWithImpl<$Res, City>;
+  @useResult
+  $Res call({String id, String name});
+}
+
+/// @nodoc
+class _$CityCopyWithImpl<$Res, $Val extends City>
+    implements $CityCopyWith<$Res> {
+  _$CityCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_CityCopyWith<$Res> implements $CityCopyWith<$Res> {
+  factory _$$_CityCopyWith(_$_City value, $Res Function(_$_City) then) =
+      __$$_CityCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String id, String name});
+}
+
+/// @nodoc
+class __$$_CityCopyWithImpl<$Res> extends _$CityCopyWithImpl<$Res, _$_City>
+    implements _$$_CityCopyWith<$Res> {
+  __$$_CityCopyWithImpl(_$_City _value, $Res Function(_$_City) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+  }) {
+    return _then(_$_City(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_City implements _City {
+  _$_City({required this.id, required this.name});
+
+  factory _$_City.fromJson(Map<String, dynamic> json) => _$$_CityFromJson(json);
+
+  @override
+  final String id;
+  @override
+  final String name;
+
+  @override
+  String toString() {
+    return 'City(id: $id, name: $name)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_City &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, name);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_CityCopyWith<_$_City> get copyWith =>
+      __$$_CityCopyWithImpl<_$_City>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_CityToJson(
+      this,
+    );
+  }
+}
+
+abstract class _City implements City {
+  factory _City({required final String id, required final String name}) =
+      _$_City;
+
+  factory _City.fromJson(Map<String, dynamic> json) = _$_City.fromJson;
+
+  @override
+  String get id;
+  @override
+  String get name;
+  @override
+  @JsonKey(ignore: true)
+  _$$_CityCopyWith<_$_City> get copyWith => throw _privateConstructorUsedError;
+}
