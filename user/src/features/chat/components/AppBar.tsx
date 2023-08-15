d82abdd95c5946667129317a8fde74965930808b
Islam Naasani <islamnaasani@gmail.com>
2023-08-16 01:21:38 +0300

    [user](chat): improve UI

--- user/src/features/chat/components/AppBar.tsx
+++ user/src/features/chat/components/AppBar.tsx
@@ -1,34 +1,36 @@
-import { Tab } from "@mui/material";
+import { Paper, Tab } from "@mui/material";
 import TabPanel from "components/layout/TabPanel";
 import Tabs from "components/layout/Tabs";
 import SwipeableViews from "lib/SwipeableViews";
 import { FC, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
 import BuyChat from "./buy/BuyChat";
-import ChatBubbleOutlinedIcon from "@mui/icons-material/ChatBubbleOutlined";
 import SellChat from "./sell/SellChat";
 const AppBarChat: FC<{}> = ({}) => {
   const { t } = useTranslation("chat");
   const [activeIndex, setActiveIndex] = useState(0);
-
+  const { id } = useParams();
   const handleTabChange = (index: number) => {
     setActiveIndex(index);
   };
   return (
     <>
-      <Tabs
-        sx={{
-          bgcolor: "#fff",
-          width: { xs: 1, md: 1 },
-          px: { md: "30%" },
-          mx: "auto",
-        }}
-        value={activeIndex}
-        onChange={(_, index) => handleTabChange(index)}
-      >
-        <Tab label={t("myPurchases")} icon={<ChatBubbleOutlinedIcon />} iconPosition="start" />
-        <Tab label={t("mySales")} icon={<ChatBubbleOutlinedIcon />} iconPosition="start" />
-      </Tabs>
+      <Paper>
+        <Tabs
+          sx={{
+            bgcolor: "#fff",
+            width: { xs: 1, md: 1 },
+            px: { md: id ? 1 : "30%" },
+            mx: "auto",
+          }}
+          value={activeIndex}
+          onChange={(_, index) => handleTabChange(index)}
+        >
+          <Tab label={t("myPurchases")} />
+          <Tab label={t("mySales")} />
+        </Tabs>
+      </Paper>
       <SwipeableViews index={activeIndex} onChangeIndex={handleTabChange}>
         <TabPanel activeIndex={activeIndex} index={0}>
           <BuyChat />
