677de0faa1865f9ff27cedef532d053e7360a810
Ibrahim Al Assi <ibrahimalassi8@gmail.com>
2023-08-17 16:07:58 +0300

    fix product min

--- API/types/payment.types.ts
+++ API/types/payment.types.ts
@@ -1,10 +1,12 @@
 import { Document, Model, ObjectId, PopulatedDoc } from "mongoose";
 import { IProduct } from "./product.types";
 import { IUser } from "./user.types";
+import { ICoupon } from "./coupon.types";
 
 export interface IPayment {
     product: PopulatedDoc<Document<ObjectId>> & IProduct;
     customer: PopulatedDoc<Document<ObjectId>> & IUser;
+    coupon: PopulatedDoc<Document<ObjectId>> & ICoupon;
     price: number;
     is_discount: boolean;
 }
