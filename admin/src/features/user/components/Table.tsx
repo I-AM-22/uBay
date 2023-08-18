03fb32add7f8988e59db926453392521a4625611
Islam Naasani <islamnaasani@gmail.com>
2023-08-18 23:36:09 +0300

    [admin](user): add wallet to tablel

--- admin/src/features/user/components/Table.tsx
+++ admin/src/features/user/components/Table.tsx
@@ -17,6 +17,7 @@ import useQuerySearchParam from "hooks/useQuerySearchParam";
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { getPage } from "utils/apiHelpers";
+import { priceFormatter } from "utils/transforms";
 import { User, userQueries } from "..";
 import useTableHeader from "../hooks/useTableHeaders";
 type Props = { setUserToDeposit: React.Dispatch<React.SetStateAction<User | null>> };
@@ -74,6 +75,8 @@ export const Table: FC<Props> = ({ setUserToDeposit }) => {
               </Stack>
             </TableCell>
             <TableCell>{row.email}</TableCell>
+            <TableCell>{row.wallet && priceFormatter.format(row.wallet.available)}</TableCell>
+            <TableCell>{row.wallet && priceFormatter.format(row.wallet.pending)}</TableCell>
             <TableCell>
               <LTR>{dayjs(row.createdAt).format("YYYY/MM/DD hh:mm A")}</LTR>
             </TableCell>
