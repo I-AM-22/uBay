de51a79f662ed2e779f0a3d554be1a187b7a2a96
majed alaswad <alaswadmajed389@gmail.com>
2023-08-19 16:53:07 +0300

    [warehouse](APP):fix getAllProduct

--- warehouse/lib/core/strings/end_points.dart
+++ warehouse/lib/core/strings/end_points.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: constant_identifier_names
 
-const String BASE_URL = 'http://192.168.1.101:3000/api/v1';
+const String BASE_URL = 'http://192.168.1.103:3000/api/v1';
 const String LOGIN = '/employees/login';
 const String RECEIVE_DELIVERY='/deliveries/receive';
 const String GET_PRODUCT='/products/';
--- warehouse/lib/features/product/presentation/widget/get_product_widget.dart
+++ warehouse/lib/features/product/presentation/widget/get_product_widget.dart
@@ -1,3 +1,4 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:warehouse/core/strings/id_and_token.dart';
@@ -7,55 +8,70 @@ import 'package:warehouse/core/widget/loading_widget.dart';
 import 'package:warehouse/features/product/data/model/product_model/product_model.dart';
 import 'package:warehouse/features/product/presentation/bloc/product_bloc.dart';
 import 'package:warehouse/injection_container.dart' as di;
-
+import 'package:intl/intl.dart';
 import '../../../../core/theme.dart';
+import '../../../../core/util/chose_date_time.dart';
 
 class GetProductWidget extends StatelessWidget {
   final String payment;
   final String product;
   final bool isDeliver;
 
-  const GetProductWidget({super.key, required this.payment, required this.product, required this.isDeliver});
+  GetProductWidget(
+      {super.key, required this.payment, required this.product, required this.isDeliver});
 
+  var formatter = NumberFormat('###,###,###,000');
+  bool isLoading = false;
 
 
   @override
   Widget build(BuildContext context) {
-    String message=isDeliver?'تم استلام القطعة بنجاح':'تم تسليم القطعة للمشتري بنجاح';
-    String bottom=isDeliver?'تأكيد استلام القطعة':'تأكيد تسليم القطعة';
-    String store=isDeliver?'هذه القطعة لا يجب تسليمها لمستودعنا':'هذه القطعة ليست في مستودعنا';
+    String message = isDeliver
+        ? 'تم استلام القطعة بنجاح'
+        : 'تم تسليم القطعة للمشتري بنجاح';
+    String bottom = isDeliver ? 'تأكيد استلام القطعة' : 'تأكيد تسليم القطعة';
+    String store = isDeliver
+        ? 'هذه القطعة لا يجب تسليمها لمستودعنا'
+        : 'هذه القطعة ليست في مستودعنا';
     return BlocProvider(
       create: (_) =>
-          di.getIt<ProductBloc>()..add(ProductEvent.getProductEvent(product)),
+      di.getIt<ProductBloc>()
+        ..add(ProductEvent.getProductEvent(product)),
       child: BlocConsumer<ProductBloc, ProductState>(
           listener: (context, state) {
             state.maybeWhen(
                 orElse: () {},
+                loadingReceiveOrGive: () {
+                  isLoading = true;
+                },
                 errorGetProductState: (message) {
+                  isLoading=false;
                   SnackBarMessage().snackBarMessageError(context, message);
                 },
-              successReceiveProductState: (){
-                SnackBarMessage().snackBarMessageSuccess(context, message);
-                 Navigator.pushNamedAndRemoveUntil(context, '/homePage', (route) => false);
-
-              },
-              errorReceiveProductState: (message){
+                successReceiveProductState: () {
+                  SnackBarMessage().snackBarMessageSuccess(context, message);
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, '/homePage', (route) => false);
+                },
+                errorReceiveProductState: (message) {
                   SnackBarMessage().snackBarMessageError(context, message);
-              }
+                }
             );
           },
-          builder: (context, state) => state.maybeWhen(
-              orElse: () => const Center(child: Text('No Data')),
-              successGetProductState: (productModel) =>
-                  _buildProduct(productModel, context,bottom,store),
-              loading: () => const LoadingWidget())),
+          builder: (context, state) =>
+              state.maybeWhen(
+                  orElse: () => const Center(child: Text('خطأ غير معروف, الرجاء المحاولة لاحقاً')),
+                  successGetProductState: (productModel) =>
+                      _buildProduct(productModel, context, bottom, store),
+                  loading: () => const LoadingWidget())),
     );
   }
 
-  Widget _buildProduct(ProductModel productModel, BuildContext context,String bottom,String store) {
+  Widget _buildProduct(ProductModel productModel, BuildContext context,
+      String bottom, String store) {
     return Column(
       children: [
-        if(idStore!=productModel.store.id)...[
+        if(idStore != productModel.store.id)...[
           Card(
             color: primaryColor1,
             shape: RoundedRectangleBorder(
@@ -68,7 +84,7 @@ class GetProductWidget extends StatelessWidget {
                 children: [
                   Text(store,),
                   const SizedBox(width: 5,),
-                  const Icon(Icons.error_outline,color: Colors.red,)
+                  const Icon(Icons.error_outline, color: Colors.red,)
                 ],
               ),
             ),
@@ -88,29 +104,75 @@ class GetProductWidget extends StatelessWidget {
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        productModel.user.name,
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.bold),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(borderRadius),
+                        ),
+                        color: primaryColor,
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            '${formatter.format(productModel.price)} ',
+                            style: Theme
+                                .of(context)
+                                .textTheme
+                                .titleSmall!
+                                .copyWith(color: Colors.white),
+                          ),
+                        ),
                       ),
                       const SizedBox(
                         width: 10,
                       ),
-                      CircleAvatar(
-                        radius: 25,
-                        backgroundImage: NetworkImage(productModel.user.photo),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            productModel.user.name,
+                            style: Theme
+                                .of(context)
+                                .textTheme
+                                .bodyLarge!
+                                .copyWith(fontSize: 18),
+                          ),
+                          Text(
+                              ChoseDateTime().chose(
+                                  productModel.createdAt),
+                              style: Theme
+                                  .of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(
+                                  fontSize: 13, color: Colors.grey))
+                        ],
                       ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      CircleAvatar(
+                          radius: 25,
+                          backgroundImage: NetworkImage(
+                              productModel.user.photo)),
                     ],
                   ),
                   const Divider(),
                   Text(
                     productModel.title,
-                    style: Theme.of(context).textTheme.bodyLarge,
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .bodyLarge,
                   ),
                   const SizedBox(height: 10,),
                   Text(
                     productModel.content,
-                    style: Theme.of(context).textTheme.bodyMedium,
+                    textAlign: TextAlign.end,
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .bodyMedium,
                   ),
                   const SizedBox(
                     height: 10,
@@ -118,33 +180,43 @@ class GetProductWidget extends StatelessWidget {
                   ListView.separated(
                       shrinkWrap: true,
                       physics: const BouncingScrollPhysics(),
-                      itemBuilder: (context, index) => Image(
+                      itemBuilder: (context, index) =>
+                          Image(
 
-                          image: NetworkImage(productModel.photos[index])),
-                      separatorBuilder: (context, index) => const SizedBox(
-                            height: 10,
-                          ),
+                              image: NetworkImage(productModel.photos[index])),
+                      separatorBuilder: (context, index) =>
+                      const SizedBox(
+                        height: 10,
+                      ),
                       itemCount: productModel.photos.length),
                   const SizedBox(
                     height: 16,
                   ),
-                  ElevatedButtonWidget(
-                      onPressed: () {
-                        BlocProvider.of<ProductBloc>(context)
-                            .add(ProductEvent.receiveProductEvent(payment,isDeliver?'0':'1'));
-                      },
-                      row: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            bottom,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(color: Colors.white),
-                          )
-                        ],
-                      )),
+                  ConditionalBuilder(
+                    builder: (context) =>
+                        ElevatedButtonWidget(
+                          color: primaryColor,
+                            onPressed: () {
+                              BlocProvider.of<ProductBloc>(context)
+                                  .add(ProductEvent.receiveProductEvent(
+                                  payment, isDeliver ? '0' : '1'));
+                            },
+                            widget: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  bottom,
+                                  style: Theme
+                                      .of(context)
+                                      .textTheme
+                                      .bodyMedium!
+                                      .copyWith(color: Colors.white),
+                                )
+                              ],
+                            )),
+                    fallback: (context) => ElevatedButtonWidget(onPressed: (){}, widget: LoadingWidget(),color: Colors.grey,),
+                    condition:!isLoading,
+                  ),
                 ],
               ),
             ),
