abe725593cb00a9e40feba1bcc724e4c89562b2e
Amr Kallas <amrkallas8@gmail.com>
2023-08-22 17:36:51 +0300

    some edit in translate

--- admin/src/components/buttons/ShowIconButton.tsx
+++ admin/src/components/buttons/ShowIconButton.tsx
@@ -4,9 +4,9 @@ import { FC } from "react";
 import { useTranslation } from "react-i18next";
 type Props = IconButtonProps;
 const ShowIconButton: FC<Props> = (props) => {
-  const { t } = useTranslation();
+  const { t } = useTranslation("user");
   return (
-    <Tooltip title={t("details")}>
+    <Tooltip title={t("details.title")}>
       <IconButton {...props}>
         <VisibilityIcon sx={{ color: "grey.700" }} />
       </IconButton>
