7720626dd0ae91d068122032203e0bd5943f4b18
Amr Kallas <amrkallas8@gmail.com>
2023-08-22 15:27:17 +0300

    end translate user

--- user/src/localization/en/index.ts
+++ user/src/localization/en/index.ts
@@ -3,5 +3,23 @@ import auth from "./auth.json";
 import common from "./common.json";
 import layout from "./layout.json";
 import validation from "./validation.json";
-const language = { common, validation, layout, auth, zod } as const;
+import account from "./account.json";
+import chat from "./chat.json";
+import comment from "./comment.json";
+import discount from "./discount.json";
+import payment from "./payment.json";
+import post from "./post.json";
+const language = {
+  common,
+  validation,
+  layout,
+  auth,
+  zod,
+  account,
+  chat,
+  comment,
+  discount,
+  payment,
+  post,
+} as const;
 export default language;
