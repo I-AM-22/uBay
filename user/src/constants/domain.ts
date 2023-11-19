177339f2d07f8dcae8e480c479614954f54973db
Islam Naasani <islamnaasani@gmail.com>
2023-11-19 12:02:55 +0300

    [user]: deploy to vercel

--- user/src/constants/domain.ts
+++ user/src/constants/domain.ts
@@ -1,5 +1,8 @@
 const DEVELOPMENT_BASE_URL = `${window.location.origin.split(":").slice(0, 2).join(":")}:3000`;
 const DEVELOPMENT_API_BASE_URL = `${DEVELOPMENT_BASE_URL}/api/v1`;
 
-export const SERVER_BASE_URL = DEVELOPMENT_BASE_URL;
-export const API_BASE_URL = DEVELOPMENT_API_BASE_URL;
+const PRODUCTION_BASE_URL = `https://ubay-9pwo.onrender.com`;
+const PRODUCTION_API_BASE_URL = `${PRODUCTION_BASE_URL}/api/v1`;
+
+export const SERVER_BASE_URL = PRODUCTION_BASE_URL;
+export const API_BASE_URL = PRODUCTION_API_BASE_URL;
