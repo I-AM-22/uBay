360ad8d1e8b275b3d86d86381cc1adc112f4c450
Islam Naasani <islamnaasani@gmail.com>
2025-01-04 23:06:32 +0300

    [admin]: update API route

--- admin/src/constants/domain.ts
+++ admin/src/constants/domain.ts
@@ -1,7 +1,7 @@
 const DEVELOPMENT_BASE_URL = `${window.location.origin.split(":").slice(0, 2).join(":")}:3000`;
 const DEVELOPMENT_API_BASE_URL = `${DEVELOPMENT_BASE_URL}/api/v1`;
 
-const PRODUCTION_BASE_URL = `https://ubay-9pwo.onrender.com`;
+const PRODUCTION_BASE_URL = `https://ubay-u4rp.onrender.com`;
 const PRODUCTION_API_BASE_URL = `${PRODUCTION_BASE_URL}/api/v1`;
 
 export const SERVER_BASE_URL = PRODUCTION_BASE_URL;
